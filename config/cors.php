93ebd3b226287c924d71fdb9e49b95d0b9a50d0f
nafithaprism <bilal@prism-me.com>
2025-09-18 10:34:19 +0400

    Update cors.php

--- config/cors.php
+++ config/cors.php
@@ -1,13 +1,14 @@
 <?php
-
 return [
     'paths' => ['v1/*', 'api/*', 'sanctum/csrf-cookie'],
-    'allowed_methods' => ['*'],
-    'allowed_origins' => ['*'],
-    'allowed_origins_patterns' => ['*'],
-    'allowed_headers' => [''],
-    'exposed_headers' => [],
-    'max_age' => 0,
-    'supports_credentials' => false,
-
+    'allowed_methods' => ['GET','POST','PUT','PATCH','DELETE','OPTIONS'],
+    'allowed_origins' => [
+        'https://medi-express.prismcloudhosting.com',
+        'https://cms-medi-express.prismcloudhosting.com',
+    ],
+    'allowed_origins_patterns' => [],           // remove the wildcard pattern
+    'allowed_headers' => ['*'],                 // or list explicitly if you prefer
+    'exposed_headers' => ['ETag'],
+    'max_age' => 86400,
+    'supports_credentials' => false,            // set true ONLY if you actually use cookies
 ];
