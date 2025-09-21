62a38692d3673e0a623312eb3ebea962d3b1a68e
nafithaprism <bilal@prism-me.com>
2025-09-21 19:03:42 +0400

    Create ForceCorsHeaders.php

---
+++ app/Http/Middleware/ForceCorsHeaders.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class ForceCorsHeaders
+{
+    public function handle(Request $request, Closure $next): Response
+    {
+        /** @var \Symfony\Component\HttpFoundation\Response $response */
+        $response = $next($request);
+
+        $origin = $request->headers->get('Origin');
+        $allowed = [
+            'https://medi-express.prismcloudhosting.com',
+            'https://cms-medi-express.prismcloudhosting.com',
+        ];
+
+        if ($origin && in_array($origin, $allowed, true)) {
+            $response->headers->set('Access-Control-Allow-Origin', $origin);
+            $response->headers->set('Vary', 'Origin');
+        }
+
+        return $response;
+    }
+}
