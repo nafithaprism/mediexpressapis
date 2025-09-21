57ff0a5c72693b5a0e05098d40bd308d8d82df38
nafithaprism <bilal@prism-me.com>
2025-09-21 19:20:15 +0400

    Update cors.php

--- config/cors.php
+++ config/cors.php
@@ -12,10 +12,37 @@
 
 // ];
 
+// <?php
+
+// return [
+//     'paths' => ['v1/', 'api/', 'sanctum/csrf-cookie'],
+
+//     'allowed_methods' => ['*'],
+
+//     'allowed_origins' => [
+//         'https://medi-express.prismcloudhosting.com',
+//         'https://cms-medi-express.prismcloudhosting.com',
+//     ],
+
+ 
+//     'allowed_origins_patterns' => [],
+
+  
+//     'allowed_headers' => ['*'],
+
+//     'exposed_headers' => [],
+//     'max_age' => 86400, 
+//     'supports_credentials' => false,
+// ];
+
+
+
+
 <?php
 
 return [
-    'paths' => ['v1/', 'api/', 'sanctum/csrf-cookie'],
+    // Use wildcards so CORS applies to nested endpoints
+    'paths' => ['v1/*', 'api/*', 'sanctum/csrf-cookie'],
 
     'allowed_methods' => ['*'],
 
@@ -24,13 +51,13 @@ return [
         'https://cms-medi-express.prismcloudhosting.com',
     ],
 
- 
     'allowed_origins_patterns' => [],
 
-  
+    // '*' is fine if you’re not using credentials
     'allowed_headers' => ['*'],
 
     'exposed_headers' => [],
-    'max_age' => 86400, 
+    'max_age' => 86400,
     'supports_credentials' => false,
 ];
+
