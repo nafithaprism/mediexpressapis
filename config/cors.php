b4d2997fa0f65a15c39b595908be5146bd3dd0af
nafithaprism <bilal@prism-me.com>
2025-09-21 20:44:43 +0400

    updated cors

--- config/cors.php
+++ config/cors.php
@@ -7,9 +7,6 @@ return [
     'allowed_methods' => ['*'],
 
     'allowed_origins' => [
-        'http://localhost:5173',
-        'http://127.0.0.1:5173',
-        'http://localhost:3000',
         'https://medi-express.prismcloudhosting.com',
         'https://cms-medi-express.prismcloudhosting.com',
     ],
