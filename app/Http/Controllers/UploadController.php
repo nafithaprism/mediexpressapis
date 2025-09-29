324927d06d7bab787359fbcc1d0165f1ede04ec6
nafithaprism <bilal@prism-me.com>
2025-09-29 10:15:42 +0400

    updated laravel apis

--- app/Http/Controllers/UploadController.php
+++ app/Http/Controllers/UploadController.php
@@ -94,31 +94,73 @@ class UploadController extends Controller
     }
 
 
-    public function update_image($file, $id)
-    {
+    // public function update_image($file, $id)
+    // {
 
-        $existing_data = Upload::select('name')->where('id', $id)->first();
-        $existing_name = $existing_data->name;
+    //     $existing_data = Upload::select('name')->where('id', $id)->first();
+    //     $existing_name = $existing_data->name;
 
-        $without_ext_name = $this->slugify(preg_replace('/\..+$/', '', $file->getClientOriginalName()));
+    //     $without_ext_name = $this->slugify(preg_replace('/\..+$/', '', $file->getClientOriginalName()));
 
-        $name = $without_ext_name . '-' . time() . rand(1, 100) . '.' . $file->extension();
-        $files[$i]['name'] = $name;
-        $files[$i]['url'] = $this->base_URL . $name;
-        $files[$i]['alt_tag'] = time() . rand(1, 100);
+    //     $name = $without_ext_name . '-' . time() . rand(1, 100) . '.' . $file->extension();
+    //     $files[$i]['name'] = $name;
+    //     $files[$i]['url'] = $this->base_URL . $name;
+    //     $files[$i]['alt_tag'] = time() . rand(1, 100);
 
-        if ($this->bunnyCDNStorage->uploadFile($file->getPathName(), $this->storageZone . "/images/{$name}")) {
+    //     if ($this->bunnyCDNStorage->uploadFile($file->getPathName(), $this->storageZone . "/images/{$name}")) {
 
-            $isUpdated = Upload::where('_id', $id)->update(['url' => $name, 'avatar' => $files[$i]['url']]);
+    //         $isUpdated = Upload::where('_id', $id)->update(['url' => $name, 'avatar' => $files[$i]['url']]);
 
-            if (!$this->bunnyCDNStorage->deleteObject('/medi-expresss/images/' . $existing_name)) {
-                echo json_encode(['message' => 'Bucket error', 'status' => 404]);
-            }
-        } else {
+    //         if (!$this->bunnyCDNStorage->deleteObject('/medi-expresss/images/' . $existing_name)) {
+    //             echo json_encode(['message' => 'Bucket error', 'status' => 404]);
+    //         }
+    //     } else {
 
-            return $errors = ['message' => 'server issue', 'status' => 404, 'image_name' => $file->getClientOrignalName()];
-        }
+    //         return $errors = ['message' => 'server issue', 'status' => 404, 'image_name' => $file->getClientOrignalName()];
+    //     }
+    // }
+public function update_image(Request $request, int $id)
+{
+    $file = $request->file('image');
+    if (!$file) {
+        return response()->json(['message' => 'No image provided', 'status' => 422], 422);
     }
+
+    // get the current stored object name (not the full URL)
+    $existing = Upload::select('url')->find($id);
+    if (!$existing) {
+        return response()->json(['message' => 'Record not found', 'status' => 404], 404);
+    }
+
+    // build new name & paths
+    $withoutExt = $this->slugify(preg_replace('/\..+$/', '', $file->getClientOriginalName()));
+    $name       = $withoutExt . '-' . time() . rand(1, 100) . '.' . $file->extension();
+
+    // storage key must match how you upload elsewhere: "<zone>/images/<name>"
+    $newStorageKey = $this->storageZone . "/images/{$name}";
+    $newPublicUrl  = rtrim($this->base_URL, '/') . '/' . $name;
+
+    // upload replacement
+    $ok = $this->bunnyCDNStorage->uploadFile($file->getPathname(), $newStorageKey);
+    if (!$ok) {
+        return response()->json(['message' => 'Upload failed', 'status' => 502], 502);
+    }
+
+    // update DB
+    Upload::where('id', $id)->update([
+        'url'    => $name,        // stored object name
+        'avatar' => $newPublicUrl // full CDN URL
+    ]);
+
+    // delete old object (key is relative to the same zone/prefix style)
+    $oldName = $existing->url;
+    if (!empty($oldName)) {
+        $this->bunnyCDNStorage->deleteObject($this->storageZone . "/images/{$oldName}");
+    }
+
+    return response()->json(['message' => 'Image updated', 'status' => 200]);
+}
+
 
     public function delete_images(Upload $upload)
     {
